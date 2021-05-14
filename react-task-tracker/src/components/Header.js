2f332d03e502426782472e65a0dfd41cc41accd1
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-14 22:54:54 +1000

    updated index.css

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,28 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+
+const Header = ({title}) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{title}</h1>
         </header>
     )
+}
+
+// Sets a default value, in case prop is not entered.
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+// Restricts the prop data type. e.g. 'string', 'boolean', etc
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+// CSS in js.
+var headingStyle = {
+    color: 'blue',
+    backgroundColor: 'gray'
 }
 
 export default Header
