204f7e256628a9419b3edc1cefe2571078fb53eb
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-05-15 21:44:30 +1000

    implemented feature that allows us to set reminders by double clicking task - style update when reminder is set to true

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -29,10 +29,15 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder} : task))
+    console.log(tasks.find(task => task.id === id))
+  }
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks' }
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks' }
     </div>
   );
 }
